92ac91fa3a3b8ae2397c297ae92179150cec7801
Conor Tompkins <conorotompkins@gmail.com>
2020-10-01 12:23:02 -0400

    added code to save aggregated csv file, cleaned up filtering and aggregation steps, added labels and improved legends on graphs

--- scripts/covid/covid_parking_analysis.R
+++ scripts/covid/covid_parking_analysis.R
@@ -14,79 +14,89 @@ glimpse(data)
 
 data %>% 
   distinct(zone) %>% 
-  View()
+  arrange(zone)
+  
+# df_ts <- data %>%
+#   select(start, meter_transactions, mobile_transactions) %>%
+#   separate(start, into = c("start_date", "start_time"), remove = TRUE, sep = " ") %>%
+#   mutate(start_date = ymd(start_date)) %>%
+#   group_by(start_date) %>%
+#   summarize(meter_transactions = sum(meter_transactions),
+#             mobile_transactions = sum(mobile_transactions)) %>%
+#   ungroup() %>%
+#   rowwise() %>%
+#   mutate(total_parking_transactions = meter_transactions + mobile_transactions) %>%
+#   ungroup() %>%
+#   mutate(year = year(start_date),
+#          day_of_year = yday(start_date),
+#          week_of_year = week(start_date),
+#          weekday = wday(start_date, label = TRUE)) %>%
+#   select(start_date, year, week_of_year, day_of_year, weekday, everything())
+# 
+# df_ts %>%
+#   write_csv("data/summarized_parking_data.csv")
 
-df_ts <- data %>% 
-  select(start, meter_transactions, mobile_transactions) %>% 
-  separate(start, into = c("start_date", "start_time"), remove = TRUE, sep = " ") %>% 
-  mutate(start_date = ymd(start_date))
-
-df_ts <- df_ts %>% 
-  group_by(start_date) %>% 
-  summarize(meter_transactions = sum(meter_transactions),
-            mobile_transactions = sum(mobile_transactions)) %>% 
-  ungroup()
-
-df_ts <- df_ts %>% 
-  rowwise() %>% 
-  mutate(total_parking_events = meter_transactions + mobile_transactions) %>% 
-  ungroup()
-
-df_ts %>% 
-  ggplot(aes(start_date, total_parking_events)) +
-  geom_point()
-
-df_ts <- df_ts %>% 
-  mutate(year = year(start_date),
-         day_of_year = yday(start_date),
-         week_of_year = week(start_date),
-         weekday = wday(start_date, label = TRUE)) %>% 
-  select(start_date, day_of_year, week_of_year, weekday, everything())
+df_ts <- read_csv("data/summarized_parking_data.csv")
 
 df_ts %>% 
-  select(year, week_of_year, total_parking_events) %>% 
+  ggplot(aes(start_date, total_parking_transactions)) +
+  geom_point(alpha = .2, size = .5) +
+  labs(x = "Year",
+       y = "Total parking transactions") +
+  scale_y_comma()
+
+df_ts %>% 
+  select(year, week_of_year, total_parking_transactions) %>% 
   group_by(year, week_of_year) %>% 
-  summarize(total_parking_events = sum(total_parking_events)) %>% 
+  summarize(total_parking_transactions = sum(total_parking_transactions)) %>% 
   group_by(week_of_year) %>% 
-  mutate(week_median_parking_events = median(total_parking_events)) %>% 
+  mutate(week_median_parking_events = median(total_parking_transactions)) %>% 
   ungroup() %>% 
-  mutate(year_type = case_when(year == 2020 ~ "2020",
+  mutate(period = case_when(year == 2020 ~ "2020",
                                year < 2020 ~ "Before times")) %>% 
-  ggplot(aes(x = week_of_year, y = total_parking_events, color = year_type, group = year)) +
+  ggplot(aes(x = week_of_year, y = total_parking_transactions, color = period, group = year)) +
   geom_line() +
-  geom_line(aes(y = week_median_parking_events), color = "black", size = 1) +
-  scale_color_manual(values = c("red", "grey"))
+  #geom_line(aes(y = week_median_parking_events), color = "black", size = 1) +
+  scale_color_manual(values = c("red", "grey")) +
+  labs(x = "Week of year",
+       y = "Total parking events",
+       color = "Period")
 
 #2020 vs historical
 data_historical <- df_ts %>% 
   filter(start_date < "2020-01-01") %>% 
-  select(year, week_of_year, total_parking_events) %>% 
+  select(year, week_of_year, total_parking_transactions) %>% 
   group_by(year, week_of_year) %>% 
-  summarize(total_parking_events = sum(total_parking_events)) %>% 
+  summarize(total_parking_transactions = sum(total_parking_transactions)) %>% 
   group_by(week_of_year) %>% 
-  summarize(median_historical_events = median(total_parking_events),
+  summarize(median_historical_transactions = median(total_parking_transactions),
             day_count = n()) %>% 
   ungroup()
 
 data_2020 <- df_ts %>% 
-  select(start_date, week_of_year, total_parking_events) %>% 
+  select(start_date, week_of_year, total_parking_transactions) %>% 
   filter(start_date >= "2020-01-01",
+         #remove current week of data
          week_of_year < week(Sys.Date())) %>% 
   group_by(week_of_year) %>% 
-  summarize(total_parking_events = sum(total_parking_events))
+  summarize(total_parking_transactions = sum(total_parking_transactions))
 
 df <- data_2020 %>% 
   left_join(data_historical)
 
 df %>% 
-  mutate(pct_difference = (total_parking_events - median_historical_events) / median_historical_events) %>% 
+  glimpse()
+
+df %>% 
+  mutate(pct_difference = (total_parking_transactions - median_historical_transactions) / median_historical_transactions) %>% 
   ggplot(aes(week_of_year, pct_difference)) +
   geom_jitter(alpha = .3) +
-  geom_smooth(span = .4, color = "black")
+  geom_smooth(span = .4, color = "black") +
+  scale_y_percent()
 
 #analyze weekday vs weekend difference per week, historical vs 2020
 df_ts %>% 
-  select(start_date, week_of_year, weekday, total_parking_events) %>% 
+  select(start_date, week_of_year, weekday, total_parking_transactions) %>% 
   mutate(period = case_when(start_date >= "2020-01-01" ~ "2020",
                             start_date < "2020-01-01" ~ "Before times"),
          is_weekend = case_when(weekday %in% c("Sat", "Sun") ~ "weekend",
@@ -94,10 +104,12 @@ df_ts %>%
   mutate(period = fct_relevel(period, "Before times"),
          is_weekend = fct_relevel(is_weekend, "weekday")) %>% 
   group_by(period, is_weekend) %>% 
-  summarize(total_parking_events = sum(total_parking_events)) %>% 
-  mutate(pct_of_parking_events = total_parking_events / sum(total_parking_events)) %>% 
-  ggplot(aes(period, pct_of_parking_events, fill = is_weekend)) +
+  summarize(total_parking_transactions = sum(total_parking_transactions)) %>% 
+  mutate(pct_of_parking_transactions = total_parking_transactions / sum(total_parking_transactions)) %>% 
+  ggplot(aes(period, pct_of_parking_transactions, fill = is_weekend)) +
   geom_col(position = position_dodge(width = 1), color = "black", alpha = .8) +
   scale_y_percent() +
-  scale_fill_viridis_d()
-  
+  scale_fill_viridis_d() +
+  labs(x = NULL,
+       y = "Percent of transactions",
+       fill = "Day type")
